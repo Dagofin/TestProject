1f85f56d15292f94764a9b71770a62fc375617aa
dagofin <gavin.j.taggart@gmail.com>
2019-01-26 23:42:12 -0600

    no message

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -7,6 +7,7 @@ public class MyScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("You Found the Sausage!");
+        print("something changed");
 	}
 	
 	// Update is called once per frame
